1110fe69277a498e73d769555211548e3b3a2934
Pedro Luis <pedroluisnr22@gmail.com>
2025-04-08 10:22:06 -0500

    refactor: :hammer: create mutable transaction list

--- src/main/kotlin/com/github/pedroluis02/mydigitalwallet/WalletController.kt
+++ src/main/kotlin/com/github/pedroluis02/mydigitalwallet/WalletController.kt
@@ -3,16 +3,16 @@ package com.github.pedroluis02.mydigitalwallet
 class WalletController(initWallet: Wallet? = null) {
 
     private var _wallet: Wallet = initWallet ?: Wallet("Default")
+    private var _transactions: MutableList<Transaction> = mutableListOf()
+
     val wallet: Wallet
-        get() = _wallet
+        get() = _wallet.copy(transactions = _transactions)
 
     fun recharge(amount: Double) {
         val newBalance = _wallet.balance + amount
         val newTransaction = Transaction(amount, type = TransactionType.Recharge.name)
 
-        val transactions = _wallet.transactions.toMutableList()
-        transactions.add(newTransaction)
-
-        _wallet = _wallet.copy(balance = newBalance, transactions = transactions)
+        _transactions.add(newTransaction)
+        _wallet = _wallet.copy(balance = newBalance)
     }
 }
